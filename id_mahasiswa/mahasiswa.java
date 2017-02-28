7f4ec5746bf6937f6d8cea4a36bda5d7fdc564e0
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-02-28 19:14:51 +0700

    program id mahasiswa
    NIM : 10115302
    Nama : Agung Nugraha
    Kelas : PROLAN -12

---
+++ id_mahasiswa/mahasiswa.java
@@ -0,0 +1,10 @@
+/* Program ID Mahasiswa*/
+class mahasiswa
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Nama Lengkap : Agung Nugraha");
+		System.out.println("NIM 	 : 10115302");
+		System.out.println("Alamat 	 : Bandung");
+	}
+}
