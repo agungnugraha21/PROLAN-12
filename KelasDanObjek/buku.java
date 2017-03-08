a06f7b83264df8f86ea1e8f05eeaf02dfe48c8af
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-03-08 12:46:22 +0700

    Update buku.java

--- KelasDanObjek/buku.java
+++ KelasDanObjek/buku.java
@@ -1,5 +1,18 @@
 class buku {
 
 	String kode, judul, penerbit, pengarang;
+	
+	void tampilbuku(){
+		kode = "A001";
+		judul = "Java Is The Best Programming Language";
+		penerbit = "Tiga Serangkai";
+		pengarang = "Rinaldi Munir";
+
+		System.out.println(" Kode Buku = "+kode);
+		System.out.println(" Judul Buku = "+judul);
+		System.out.println(" Penerbit = "+penerbit);
+		System.out.println(" Pengarang = "+pengarang);
+		System.out.println(" -------------------");
+	}
 
 }
