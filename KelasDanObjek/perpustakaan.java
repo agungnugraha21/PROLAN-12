d940f1f183768e46f58046d0dbb09d8460bd5a47
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-03-08 12:45:01 +0700

    Update perpustakaan.java

--- KelasDanObjek/perpustakaan.java
+++ KelasDanObjek/perpustakaan.java
@@ -3,16 +3,7 @@ class perpustakaan{
 		peminjam borrow = new peminjam();
 		buku book = new buku();
 
-		book.kode = "A001";
-		book.judul = "Java Is The Best Programming Language";
-		book.penerbit = "Tiga Serangkai";
-		book.pengarang = "Rinaldi Munir";
-
-		System.out.println(" Kode Buku = "+book.kode);
-		System.out.println(" Judul Buku = "+book.judul);
-		System.out.println(" Penerbit = "+book.penerbit);
-		System.out.println(" Pengarang = "+book.pengarang);
-		System.out.println(" -------------------");
+		borrow.tampilpeminjam();
 		borrow.meminjamBuku();
 		
 		
