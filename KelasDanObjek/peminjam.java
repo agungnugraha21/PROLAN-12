2a57396fcc337c2a81fb7646dc1973676c5c24e4
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-03-08 12:41:54 +0700

    Update peminjam.java

--- KelasDanObjek/peminjam.java
+++ KelasDanObjek/peminjam.java
@@ -1,5 +1,16 @@
 class peminjam{
 	String nama, pekerjaan, alamat, nohp;
+	
+	void tampilpeminjam(){
+		
+		System.out.println(" << Perpustakaan >>");
+		System.out.println(" ------------------- ");
+		System.out.println(" Nama Peminjam = "+nama);
+		System.out.println(" Pekerjaan     = "+pekerjaan);
+		System.out.println(" Alamat        = "+alamat);
+		System.out.println(" No Hp         = "+nohp);
+		System.out.println(" -------------------");
+	}
 
 	public void meminjamBuku(){
 		System.out.println("\n NB : Buku Telah Dipinjam,, Mohon Kembalikan Tepat Waktu !!! ^_^");
