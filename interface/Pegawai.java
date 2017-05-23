26f3ed53e4a264b51cc4581457acb4197f96da2d
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-05-23 21:27:35 +0700

    Interface
    NIM : 10115302
    Nama : Agung Nugraha
    Kelas : Prolan-12
    Pertemuan Interface (Teori)

---
+++ interface/Pegawai.java
@@ -0,0 +1,8 @@
+public abstract class Pegawai {
+    protected String nama;
+    protected String nip;
+
+    protected abstract void penjaga();
+    protected abstract void buka();
+    protected abstract void libur(); 
+}
---
+++ interface/Telat.java
@@ -0,0 +1,5 @@
+public interface Telat {
+	public void dendaHari();
+	public void dendaMinggu();
+	public void dendaBulan();
+}
