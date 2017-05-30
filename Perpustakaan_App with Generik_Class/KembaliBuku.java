856fb985382eaf14749e23fb7474ce8bcabc3981
AGUNG NUGRAHA <agung.nugraha220597@gmail.com>
2017-05-30 23:13:16 +0700

    Generik_integration
    NIM : 10115302
    Nama : Agung Nugraha
    Kelas : Prolan 12
    Pertemuan Generik Class

---
+++ Perpustakaan_App with Generik_Class/KembaliBuku.java
@@ -0,0 +1,11 @@
+public class KembaliBuku {
+    int pengembalianBuku;
+    public void telat(){
+        TelatKembali<Integer> getBack = new TelatKembali<>();
+        
+        getBack.setTotalHari(7); 
+        pengembalianBuku = getBack.getPengembalian()-Buku.day;
+        System.out.println("\n\n Telat Pengembalian Buku Selama "+pengembalianBuku +" Hari\n Maka anda akan dikenakan sanksi sebagai mana mestinya\n\n");
+    }
+ 
+}
---
+++ Perpustakaan_App with Generik_Class/MainClass.java
@@ -0,0 +1,9 @@
+
+
+public class MainClass {
+    public static void main (String args[])
+        {
+		MenuPilihan menuu = new MenuPilihan();
+                menuu.menu();
+        }
+}
